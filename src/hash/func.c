2a879cec5dda344eb2a27aa13a6493b4d1acd6f9
Dusan Stevanovic <dusan@wire.com>
2026-08-10 09:59:22 +0200

    Ensure correct reads are performed

--- src/hash/func.c
+++ src/hash/func.c
@@ -184,17 +184,15 @@ uint32_t hash_joaat_pl_ci(const struct pl *pl)
 	}
 
 
-static uint32_t hashlittle( const void *key, size_t length, uint32_t initval)
+static uint32_t hashlittle(const void *key, size_t length, uint32_t initval)
 {
 	uint32_t a,b,c;
 	union { const void *ptr; size_t i; } u;
-
-	/* Set up the internal state */
 	a = b = c = 0xdeadbeef + ((uint32_t)length) + initval;
-
 	u.ptr = key;
 	if (HASH_LITTLE_ENDIAN && ((u.i & 0x3) == 0)) {
 		const uint32_t *k = (const uint32_t *)key;
+		const uint8_t  *k8;
 
 		while (length > 12) {
 			a += k[0];
@@ -205,46 +203,23 @@ static uint32_t hashlittle( const void *key, size_t length, uint32_t initval)
 			k += 3;
 		}
 
-#ifndef VALGRIND
+		k8  = (const uint8_t *)k;
 		switch (length) {
-
-		case 12: c+=k[2]; b+=k[1]; a+=k[0]; break;
-		case 11: c+=k[2]&0xffffff; b+=k[1]; a+=k[0]; break;
-		case 10: c+=k[2]&0xffff; b+=k[1]; a+=k[0]; break;
-		case 9 : c+=k[2]&0xff; b+=k[1]; a+=k[0]; break;
-		case 8 : b+=k[1]; a+=k[0]; break;
-		case 7 : b+=k[1]&0xffffff; a+=k[0]; break;
-		case 6 : b+=k[1]&0xffff; a+=k[0]; break;
-		case 5 : b+=k[1]&0xff; a+=k[0]; break;
-		case 4 : a+=k[0]; break;
-		case 3 : a+=k[0]&0xffffff; break;
-		case 2 : a+=k[0]&0xffff; break;
-		case 1 : a+=k[0]&0xff; break;
+		case 12: c += ((uint32_t)k8[11])<<24; /* fall through */
+		case 11: c += ((uint32_t)k8[10])<<16; /* fall through */
+		case 10: c += ((uint32_t)k8[9])<<8;   /* fall through */
+		case 9 : c += k8[8];                  /* fall through */
+		case 8 : b += ((uint32_t)k8[7])<<24;  /* fall through */
+		case 7 : b += ((uint32_t)k8[6])<<16;  /* fall through */
+		case 6 : b += ((uint32_t)k8[5])<<8;   /* fall through */
+		case 5 : b += k8[4];                  /* fall through */
+		case 4 : a += ((uint32_t)k8[3])<<24;  /* fall through */
+		case 3 : a += ((uint32_t)k8[2])<<16;  /* fall through */
+		case 2 : a += ((uint32_t)k8[1])<<8;   /* fall through */
+		case 1 : a += k8[0];
+			break;
 		case 0 : return c;
 		}
-
-#else /* make valgrind happy */
-
-		const uint8_t  *k8 = (const uint8_t *)k;
-		switch (length) {
-
-		case 12: c+=k[2]; b+=k[1]; a+=k[0]; break;
-		case 11: c+=((uint32_t)k8[10])<<16;  /* fall through */
-		case 10: c+=((uint32_t)k8[9])<<8;    /* fall through */
-		case 9 : c+=k8[8];                   /* fall through */
-		case 8 : b+=k[1]; a+=k[0]; break;
-		case 7 : b+=((uint32_t)k8[6])<<16;   /* fall through */
-		case 6 : b+=((uint32_t)k8[5])<<8;    /* fall through */
-		case 5 : b+=k8[4];                   /* fall through */
-		case 4 : a+=k[0]; break;
-		case 3 : a+=((uint32_t)k8[2])<<16;   /* fall through */
-		case 2 : a+=((uint32_t)k8[1])<<8;    /* fall through */
-		case 1 : a+=k8[0]; break;
-		case 0 : return c;
-		}
-
-#endif /* !valgrind */
-
 	}
 	else if (HASH_LITTLE_ENDIAN && ((u.i & 0x1) == 0)) {
 		const uint16_t *k = (const uint16_t *)key;
@@ -260,40 +235,25 @@ static uint32_t hashlittle( const void *key, size_t length, uint32_t initval)
 		}
 
 		k8 = (const uint8_t *)k;
-
 		switch (length) {
-
-		case 12: c+=k[4]+(((uint32_t)k[5])<<16);
-			b+=k[2]+(((uint32_t)k[3])<<16);
-			a+=k[0]+(((uint32_t)k[1])<<16);
-			break;
-		case 11: c+=((uint32_t)k8[10])<<16;     /* fall through */
-		case 10: c+=k[4];
-			b+=k[2]+(((uint32_t)k[3])<<16);
-			a+=k[0]+(((uint32_t)k[1])<<16);
-			break;
-		case 9 : c+=k8[8];                      /* fall through */
-		case 8 : b+=k[2]+(((uint32_t)k[3])<<16);
-			a+=k[0]+(((uint32_t)k[1])<<16);
-			break;
-		case 7 : b+=((uint32_t)k8[6])<<16;      /* fall through */
-		case 6 : b+=k[2];
-			a+=k[0]+(((uint32_t)k[1])<<16);
-			break;
-		case 5 : b+=k8[4];                      /* fall through */
-		case 4 : a+=k[0]+(((uint32_t)k[1])<<16);
-			break;
-		case 3 : a+=((uint32_t)k8[2])<<16;      /* fall through */
-		case 2 : a+=k[0];
-			break;
-		case 1 : a+=k8[0];
+		case 12: c += ((uint32_t)k8[11])<<24; /* fall through */
+		case 11: c += ((uint32_t)k8[10])<<16; /* fall through */
+		case 10: c += ((uint32_t)k8[9])<<8;   /* fall through */
+		case 9 : c += k8[8];                  /* fall through */
+		case 8 : b += ((uint32_t)k8[7])<<24;  /* fall through */
+		case 7 : b += ((uint32_t)k8[6])<<16;  /* fall through */
+		case 6 : b += ((uint32_t)k8[5])<<8;   /* fall through */
+		case 5 : b += k8[4];                  /* fall through */
+		case 4 : a += ((uint32_t)k8[3])<<24;  /* fall through */
+		case 3 : a += ((uint32_t)k8[2])<<16;  /* fall through */
+		case 2 : a += ((uint32_t)k8[1])<<8;   /* fall through */
+		case 1 : a += k8[0];
 			break;
 		case 0 : return c;
 		}
 	}
 	else {
 		const uint8_t *k = (const uint8_t *)key;
-
 		while (length > 12) {
 			a += k[0];
 			a += ((uint32_t)k[1])<<8;
@@ -311,27 +271,23 @@ static uint32_t hashlittle( const void *key, size_t length, uint32_t initval)
 			length -= 12;
 			k += 12;
 		}
-
-		/* all the case statements fall through */
 		switch (length) {
-
-		case 12: c+=((uint32_t)k[11])<<24;
-		case 11: c+=((uint32_t)k[10])<<16;
-		case 10: c+=((uint32_t)k[9])<<8;
-		case 9 : c+=k[8];
-		case 8 : b+=((uint32_t)k[7])<<24;
-		case 7 : b+=((uint32_t)k[6])<<16;
-		case 6 : b+=((uint32_t)k[5])<<8;
-		case 5 : b+=k[4];
-		case 4 : a+=((uint32_t)k[3])<<24;
-		case 3 : a+=((uint32_t)k[2])<<16;
-		case 2 : a+=((uint32_t)k[1])<<8;
-		case 1 : a+=k[0];
+		case 12: c += ((uint32_t)k[11])<<24; /* fall through */
+		case 11: c += ((uint32_t)k[10])<<16; /* fall through */
+		case 10: c += ((uint32_t)k[9])<<8;   /* fall through */
+		case 9 : c += k[8];                  /* fall through */
+		case 8 : b += ((uint32_t)k[7])<<24;  /* fall through */
+		case 7 : b += ((uint32_t)k[6])<<16;  /* fall through */
+		case 6 : b += ((uint32_t)k[5])<<8;   /* fall through */
+		case 5 : b += k[4];                  /* fall through */
+		case 4 : a += ((uint32_t)k[3])<<24;  /* fall through */
+		case 3 : a += ((uint32_t)k[2])<<16;  /* fall through */
+		case 2 : a += ((uint32_t)k[1])<<8;   /* fall through */
+		case 1 : a += k[0];
 			break;
 		case 0 : return c;
 		}
 	}
-
 	final(a,b,c);
 	return c;
 }
